8e925a9aa45911a9ecd131e0686bd73ba1df988d
Mina McGonigal <mcgonigal.mina@gmail.com>
2022-03-22 12:35:08 -0500

    finished aliases exercises

---
+++ aliases_exercises.sql
@@ -0,0 +1,17 @@
+USE employees;
+
+# 3 return 10 employee in a result set named 'full_name'
+SELECT CONCAT(last_name,' ',first_name) AS full_name
+FROM employees
+LIMIT 10;
+
+# 4 add DOB
+SELECT CONCAT(last_name, ' ', first_name) AS full_name, birth_date AS DOB
+FROM employees
+LIMIT 10;
+
+SELECT CONCAT (emp_no, ' - ',last_name, ', ' ,first_name) AS full_name, birth_date AS DOB
+FROM employees
+ORDER BY DOB ASC
+LIMIT 10;
+
