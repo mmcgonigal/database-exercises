1d493994f65571bfd2a351c776b784d1400a6982
Mina McGonigal <mcgonigal.mina@gmail.com>
2022-03-25 22:33:16 -0500

    extra join exercises

--- join_exercises.sql
+++ join_exercises.sql
@@ -16,4 +16,19 @@ FROM dept_manager DM JOIN  departments D
         ON DM.emp_no = E.emp_no
 WHERE YEAR(to_date) = 9999;
 
+SELECT D.dept_name AS '\Department Name' ,CONCAT(E.first_name, ' ' , E.last_name) AS '\Department Manager'
+FROM dept_manager DM JOIN departments D
+ON DM.dept_no = D.dept_no
+JOIN employees E
+ON DM.emp_no = E.emp_no
+WHERE E.gender = 'F'
+AND YEAR(DM.to_date) = 9999;
 
+SELECT title, COUNT(t.title)
+FROM titles t JOIN dept_emp de
+ ON t.emp_no = de.emp_no
+JOIN departments d
+ON de.dept_no = d.dept_no
+WHERE de.dept_no = 'd009'
+AND YEAR(de.to_date) = 9999
+GROUP BY t.title;
